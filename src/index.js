870c3e3dad5c041de07525b1289297f5b9a8409f
Quốc Bảo Ngô <ngobao2001@gmail.com>
2020-03-25 16:02:43 +0700

    Added some backend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import app from "./server";
 import { MongoClient } from "mongodb"
 
 const port = process.env.PORT || 3000
+let login = 0
+let ejs = require("ejs")
+let curEmail = ""
 
 MongoClient.connect(process.env.MFLIX_DB_URI, {
 	wtimeout: 2500,
@@ -15,25 +18,46 @@ MongoClient.connect(process.env.MFLIX_DB_URI, {
         let db = client.db("Web")
 
 		app.get("/", (req, res) => {
-			res.redirect("/home")
+			res.redirect("/index")
+        })
+
+        app.get("/logout", (req, res) => {
+            login = 0
+            db.collection("Users").updateOne({ "email": curEmail }, { $set: {"active": 0} })
+            res.redirect("/index")
         })
 
         app.post("/registerComplete", (req, res) => {
-            db.collection("Users").findOne({email: req.body.email}, (err, res) => {
-                if (res === null) { 
-                    console.log(1);
+            db.collection("Users").findOne({email: req.body.email}, (err, result) => {
+                if (result === null) { 
                     db.collection("Users").insertOne(
-                        { "email": req.body.email, "password": req.body.password }
+                        { "email": req.body.email, "password": req.body.password, "name": req.body.name }
                     )
+                    login = 1
+                    db.collection("Users").updateOne({ "email": req.body.email }, { $set: {"active": 1} })
+                    curEmail = req.body.email
+                    res.redirect("/InfoUpdate")
                 } else {
-                    console.log(0);
+                    res.render("registration", { login: login, loginFail: 0, registerFail: 1 })
                 }
             })
-            res.redirect("/")
+        })
+
+        app.post("/loginProcess", (req, res) => {
+            db.collection("Users").findOne({email: req.body.email}, (err, result) => {
+                if (result == null || result.password != req.body.password) {
+                    res.render("login", { login: login, loginFail: 1, registerFail: 0 })
+                } else {
+                    login = 1
+                    db.collection("Users").updateOne({ "email": req.body.email }, { $set: {"active": 1} } )
+                    curEmail = req.body.email
+                    res.redirect("/user")
+                }
+            })
         })
         
 		app.get("/:link", (req, res) => {
-            res.render(req.params.link)
+            res.render(req.params.link, { login: login, loginFail: 0, registerFail: 0 })
         })
 
 		app.listen(port, () => {
